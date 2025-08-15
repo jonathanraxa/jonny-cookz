0977c3f044498f83bbc9c3178d18176118a6dd66
Jonathan Raxa <jonathanraxa@gmail.com>
2025-08-14 21:49:34 -0500

    ...please someone just give me a chance...

--- app/meals/layout.jsx
+++ app/meals/layout.jsx
@@ -9,3 +9,4 @@ const MealsLayout = ({ children }) => {
 
 export default MealsLayout;
 // I need a job...I'm losing my mind...
+// writing another comment...I hate coding interviews...why won't someone just give me a chance?
