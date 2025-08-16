83ab077a8465252f9a3ede857e9616358c87db16
Jonathan Raxa <jonathanraxa@gmail.com>
2025-08-15 21:24:16 -0500

    add header component and css modules

--- app/meals/layout.jsx
+++ app/meals/layout.jsx
@@ -8,5 +8,3 @@ const MealsLayout = ({ children }) => {
 }
 
 export default MealsLayout;
-// I need a job...I'm losing my mind...
-// writing another comment...I hate coding interviews...why won't someone just give me a chance?
