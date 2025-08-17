28fd4bacfae6e824421daea1ab605cba4638ef53
Jonathan Raxa <jonathanraxa@gmail.com>
2025-08-17 13:22:46 -0500

    get individual meal and display contents

--- app/meals/[mealSlug]/page.jsx
+++ app/meals/[mealSlug]/page.jsx
@@ -1,11 +1,41 @@
-const MealPostPage = ({ params }) => {
+import Image from 'next/image';
+import { getMeal } from '@/lib/meals';
+
+import classes from './page.module.css';
+
+const MealDetailsPage = ({ params }) => {
+    const meal = getMeal(params.mealSlug);
+    meal.instructions = meal.instructions.replace(/\n/g, '<br />')
+
     return (
-        <div>
-            <h1>
-                {params.slug}
-            </h1>
-        </div>
+        <>
+            <header className={classes.header}>
+                <div className={classes.image}>
+                    <Image scr={meal.image} fill />
+                </div>
+                <div className={classes.headerText}>
+                    <h1>
+                        {meal.title}
+                    </h1>
+                    <p className={classes.creator}>
+                        by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+                    </p>
+                    <p className={classes.summary}>
+                        {meal.summary}
+                    </p>
+                </div>
+            </header>
+            <main>
+                <p className={classes.instructions} dangerouslySetInnerHTML={{
+                    __html: meal.instructions,
+                }}>
+                </p>
+            </main>
+        </>
     )
 }
 
-export default MealPostPage;
+export default MealDetailsPage;
+
+// what's this error?
+// Error: Route "/meals/[mealSlug]" used `params.mealSlug`. `params` should be awaited before using its properties. Learn more: https://nextjs.org/docs/messages/sync-dynamic-apis
