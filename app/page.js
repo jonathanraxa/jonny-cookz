a58d1ca60c3921f1f8e245da71f92692a2e535b4
Jonathan Raxa <jonathanraxa@gmail.com>
2025-08-16 21:42:59 -0500

    isolated links

--- app/page.js
+++ app/page.js
@@ -11,8 +11,8 @@ export default function Home() {
         </div>
         <div>
           <div className={classes.hero}>
-            <h1>NextLevel Food for NextLevel Foodies</h1>
-            <p>Taste & share food from all over the world.</p>
+            <h1>Food straight from Jonny</h1>
+            <p>Taste & share food you make for your family.</p>
           </div>
           <div className={classes.cta}>
             <Link href="/community">Join the Community</Link>
@@ -24,7 +24,7 @@ export default function Home() {
         <section className={classes.section}>
           <h2>How it works</h2>
           <p>
-            NextLevel Food is a platform for foodies to share their favorite
+            JonnyCookz is a platform for foodies to share their favorite
             recipes with the world. It&apos;s a place to discover new dishes, and to
             connect with other food lovers.
           </p>
