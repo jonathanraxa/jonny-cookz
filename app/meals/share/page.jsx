c6545a5a4f05b5b1225e834eeb6634ef712ab79e
Jonathan Raxa <jonathanraxa@gmail.com>
2025-08-19 21:18:48 -0500

    add loading state to button

--- app/meals/share/page.jsx
+++ app/meals/share/page.jsx
@@ -1,6 +1,7 @@
 import classes from './page.module.css';
 import ImagePicker from '../image-picker';
 import { shareMeal } from '@/lib/actions';
+import MealsFormSubmit from '@/app/meals/meals-form-submit';
 
 export default function ShareMealPage() {
     return (
@@ -42,7 +43,8 @@ export default function ShareMealPage() {
                     </p>
                     <ImagePicker label='Your image' name='image' />
                     <p className={classes.actions}>
-                        <button type="submit">Share Meal</button>
+                        {/* <button type="submit">Share Meal</button> */}
+                        <MealsFormSubmit />
                     </p>
                 </form>
             </main>
